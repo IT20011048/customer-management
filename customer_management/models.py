e453775248838788382394d138c127645b5bbbf6
jayasekara94 <it20011048@my.sliit.lk>
2021-08-23 03:58:34 +0530

    form validation

--- customer_management/models.py
+++ customer_management/models.py
@@ -1,3 +1,10 @@
+from enum import unique
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+
+class User(models.Model):
+  name = models.CharField(max_length=80, null=False,blank=False)
+  address = models.CharField(max_length=150, null=False,blank=False)
+  phone = models.PhoneNumberField(null=False, blank=False, unique=True)
+  email = models.CharField(max_length=70,null=False,blank=False)
