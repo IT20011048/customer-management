edf557997fd4ecd424d3ed52ef869a605d74df1d
IT20011048 <it20011048@my.sliit.lk>
2021-08-24 23:50:33 +0530

    right path 1

--- supermarket/customer/admin.py
+++ supermarket/customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import cus
 # Register your models here.
+
+@admin.register(cus)
+class useradmin(admin.ModelAdmin):
+    list_display = ('id','name','address','phone_number','email_address')
--- supermarket/customer/models.py
+++ supermarket/customer/models.py
@@ -1,3 +1,10 @@
+from enum import unique
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import CharField
 
-# Create your models here.
+class cus(models.Model):
+  name = models.CharField(max_length=80, blank=False, null=False)
+  address = models.CharField(max_length=150,blank=False, null=False)
+  phone_number = models.CharField(max_length=15, blank=False, null=False, unique=True,)
+  email_address = models.CharField(max_length=70,unique=True)
